495e48e8c34dcd89a28471dc1f14541197158a44
AlexNesvat <nonaname1993@gmail.com>
2019-07-17 14:39:27 +0300

    create post model/factory/migration

---
+++ database/migrations/2019_07_17_110733_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('author_id')->unsigned();// TODO: add key
+            $table->dateTime('date_of_publication');
+            $table->timestamps();
+        //пост должен содержать заголовок, описание, автор, дата публикации, изображение
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
